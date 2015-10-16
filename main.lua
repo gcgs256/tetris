be63cd70cf7d26dbd327226cc45bff793bef219c
Gayle Geschwind <gcgs256@gmail.com>
2015-10-15 23:25:21 -0400

    initializes and draws grid contents

--- main.lua
+++ main.lua
@@ -5,11 +5,13 @@ local width, height = love.window.getDimensions()
 
 function love.load()
 	love.graphics.setBackgroundColor(190, 190, 190)
+	initializeGrid()
 	
 end
 
 
 function love.draw()
+	fillGrid()
 	drawGridLines()
 end
 
@@ -26,4 +28,36 @@ function drawGridLines()
 	for i = 1, gridHeight do
 		love.graphics.line(0, (height/gridHeight)*i, width, (height/gridHeight)*i)
 	end
+end
+
+function fillGrid()
+	for i = 1, gridHeight do
+		for j = 1, gridWidth do
+			local block = gameGrid[i][j]
+			love.graphics.setColor(block.color.red, block.color.green, block.color.blue)
+			love.graphics.rectangle("fill", (j-1)*(width/gridWidth), (i-1)*(height/gridHeight), width/gridWidth, height/gridHeight)
+		end
+	end
+end
+
+function initializeGrid()
+	gameGrid = {}
+	for i = 1, gridHeight do
+		row = {}
+		for j = 1, gridWidth do
+			table.insert(row, createEmptyBlock())
+		end
+		table.insert(gameGrid, row)
+	end
+end
+
+function createEmptyBlock()
+	return {
+		empty = true,
+		color = {
+			red = 190,
+			green = 190,
+			blue = 190
+		}
+	}
 end
